a4af42eb39c25a70e483d5e84a87ff74834f3bf8
Vladimir Shestakov <boolive@yandex.ru>
2017-05-18 14:34:20 +0300

    fix Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
 import store from './store/store.js';
 import App from './views/app/App.js';
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router>
-            <App/>
-        </Router>
+        <App/>
     </Provider>,
     document.getElementById('app')
 );
