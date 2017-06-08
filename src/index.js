f9a56c876016b80e88f11b0445a4239f623c013b
Ramil Zaynetdinov <ramil.zv@gmail.com>
2017-06-08 16:42:11 +0300

    updated packages, moved the stores

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import store from './store/store.js';
+import stores from './stores';
 import App from './views/app/App.js';
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={stores}>
         <App/>
     </Provider>,
     document.getElementById('app')
--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import * as reducers from './reducers';
+import * as reducers from './store/reducers';
 //import createLogger from 'redux-logger'
 
 export default createStore(
