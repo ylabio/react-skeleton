bfbe9c0e29f1636820a08b7f74f44e18a1576108
Vladimir Shestakov <boolive@yandex.ru>
2018-01-28 23:19:20 +0300

    added component button

---
+++ src/components/elements/button/index.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import cs from 'classnames';
+import './style.less';
+
+export default class Button extends Component {
+
+  static propTypes = {
+    children: PropTypes.node,
+    onClick: PropTypes.func,
+    href: PropTypes.string,
+    title: PropTypes.string,
+    theme: PropTypes.oneOfType([PropTypes.string, PropTypes.array])
+  };
+
+  static defaultProps = {
+    onClick: () => {
+    }
+  };
+
+  onClick = (e) => {
+    e.preventDefault();
+    this.props.onClick();
+  };
+
+  themeClassNames(basename, theme) {
+    const result = {};
+    if (theme) {
+      if (typeof theme === 'string') {
+        result[basename + theme] = true;
+      }
+      if (Array.isArray(theme)) {
+        for (const item of theme) {
+          result[basename + item] = true;
+        }
+      }
+    }
+    return result;
+  }
+
+  render() {
+    const {theme, href, title} = this.props;
+    return (
+      <a
+        type="button"
+        className={cs('Button', this.themeClassNames('Button_theme_', theme))}
+        title={title}
+        href={href || '#'}
+        onClick={this.onClick}
+      >
+        {this.props.children}
+      </a>
+    );
+  }
+}
