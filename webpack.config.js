6450e5f987872052e11b26768121d944a54901f9
Vladimir Shestakov <boolive@yandex.ru>
2018-04-11 08:33:27 +0300

    update all packages, webpack 4, fix storybooks

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 let config = {
-  //mode: process.env.NODE_ENV,
+  mode: process.env.NODE_ENV,
   context: path.join(__dirname, "/src"),
   entry: [
     'babel-polyfill',
@@ -81,7 +81,7 @@ if (process.env.NODE_ENV === 'production') {
   //config.devtool = "nosources-source-map";
   //config.plugins.push(new webpack.optimize.UglifyJsPlugin());
 } else {
-  // config.entry.push('webpack-dev-server/client?http://localhost:8020');
+  // config.entry.push('webpack-dev-server/client?http://localhost:8030');
   // config.entry.push('webpack/hot/only-dev-server');
   config.devtool = 'inline-source-map';// "#cheap-module-inline-source-map";
 
@@ -91,7 +91,7 @@ if (process.env.NODE_ENV === 'production') {
   config.devServer = {
     //compress: false,
     contentBase: path.join(__dirname, "dist"),
-    port: 8010,
+    port: 8030,
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true,
