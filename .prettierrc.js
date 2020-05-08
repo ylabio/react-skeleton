ac9b08a1cb5363b21bfb29b5c3e8b5bbdc30acbb
Roman Nichepurenko <roman.nicepurenko@ylab.io>
2020-05-08 13:35:51 +0300

    Add the correct configuration for less files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-  "printWidth": 100,
-  "parser": "flow",
+  printWidth: 100,
+  parser: 'flow',
   singleQuote: true,
   arrowParens: 'avoid',
-  trailingComma: 'all'
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: '*.less',
+      options: {
+        parser: 'less',
+      },
+    },
+  ],
 };
