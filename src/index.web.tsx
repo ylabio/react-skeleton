4f4786f85085c7ab7588aa0988bec59b49d5243c
Vladimir Shestakov <boolive@yandex.ru>
2022-11-22 14:33:51 +0300

    upgrade react 18.2

--- src/index.web.tsx
+++ src/index.web.tsx
@@ -5,41 +5,44 @@
  * предотвращения начального перерендера, а также инициализируется состояние, переданное сервером.
  */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider as StoreProvider } from 'react-redux';
+import {createRoot, hydrateRoot, Root} from 'react-dom/client';
 import RouterProvider from '@src/containers/router-provider';
-import configDefault from '@src/config';
+import config from '@src/config';
 import Services from '@src/services';
 import App from '@src/app';
-import mc from 'merge-change';
 import ServicesProvider from '@src/services/provider';
 
-(async function (tagId = 'app', config = {}) {
-  // Итоговый конфиг
-  config = mc.merge(configDefault, config);
-
-  // Метод рендера по умочланию
-  let render = ReactDOM.render;
+(async function () {
 
   // Инициализация менеджера сервисов
   // Через него получаем сервисы api, navigation, store и другие
   // При первом обращении к ним, они будут автоматически инициализированы с учётом конфигурации
   const services = await new Services().init(config);
 
+  // Метод рендера по умолчанию
+  let hydrate = false;
   // Если есть подготовленные данные от SSR
   if (services.ssr.hasPreloadState()) {
     // Получаем всё состояние, с которым рендерился HTML на сервере и передаём его в сервис store
     services.store.setState(await services.ssr.getPreloadState());
     // Гидрация DOM от SSR
-    render = ReactDOM.hydrate;
+    hydrate = true;
   }
 
-  render(
+  const app = (
     <ServicesProvider services={services}>
       <RouterProvider navigation={services.navigation}>
         <App />
       </RouterProvider>
-    </ServicesProvider>,
-    document.getElementById(tagId),
+    </ServicesProvider>
   );
+
+  const dom = document.getElementById('app');
+  if (!dom) throw new Error('Failed to find the root element');
+
+  if (hydrate) {
+    hydrateRoot(dom, app);
+  } else {
+    createRoot(dom).render(app);
+  }
 })();
